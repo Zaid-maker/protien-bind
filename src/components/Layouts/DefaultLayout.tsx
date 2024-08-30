6de7e63a7704ba5755e4a153cdd44f8c5a368f1c
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-08-30 18:33:37 +0000

    props

--- src/components/Layouts/DefaultLayout.tsx
+++ src/components/Layouts/DefaultLayout.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../Sidebar";
 
 export const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
     <div className="flex">
-      <Sidebar />
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
       <div className="relative flex flex-1 flex-col lg:ml-72.5">
         <main>
           <div className="mx-auto max-w-screen-2xl p-4 dark:bg-[#121212] md:p-6 2xl:p-10">
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,6 +1,65 @@
-import React from "react";
+"use client";
 
-const Sidebar = () => {
+import React from "react";
+import {
+  LayoutGrid,
+  Atom,
+  Network,
+  Microscope,
+  Settings,
+  MessageSquareText,
+  ChevronLeft,
+} from "lucide-react";
+
+interface SidebarProps {
+  sidebarOpen: boolean;
+  setSidebarOpen: (arg: boolean) => void;
+}
+
+const menuGroups = [
+  {
+    name: "",
+    menuItems: [
+      {
+        icon: <LayoutGrid size={25} />,
+        label: "Dashboard",
+        route: "/",
+      },
+      {
+        icon: <Atom size={25} />,
+        label: "Molecules Bank",
+        route: "/molecule-bank",
+      },
+      {
+        icon: <Network size={25} />,
+        label: "Model",
+        route: "/model",
+      },
+      {
+        icon: <Microscope size={25} />,
+        label: "Research",
+        route: "/research",
+      },
+      {
+        icon: <MessageSquareText size={25} />,
+        label: "Messages",
+        route: "/message",
+      },
+    ],
+  },
+  {
+    name: "OTHERS",
+    menuItems: [
+      {
+        icon: <Settings size={25} />,
+        label: "Settings",
+        route: "/settings",
+      },
+    ],
+  },
+];
+
+const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
   return <div>Sidebar</div>;
 };
 
