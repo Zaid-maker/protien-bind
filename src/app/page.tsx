f9557b06855cc15db99384f4f58dfdf5cf2c21b0
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-08-30 18:41:22 +0000

    show DefaultLayout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-export default function Home() {
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title:
+    "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
+  description: "This is Next.js Home for TailAdmin Dashboard Template",
+};
+
+export default function Home({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <div className="h-screen">
-        <p>Default Layout</p>
-      </div>
+      <DefaultLayout>{children}</DefaultLayout>
     </>
   );
 }
