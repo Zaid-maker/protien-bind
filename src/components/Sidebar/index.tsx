6cd28f691453ae07db4696e408ae4b4d57b4a9c6
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-08-30 19:13:25 +0000

    add stylings

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -64,11 +64,15 @@ const menuGroups = [
 
 const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
   return (
-    <aside>
-      <div>
+    <aside
+      className={`fixed left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-[#000000] lg:translate-x-0 ${
+        sidebarOpen ? "translate-x-0" : "-translate-x-full"
+      }`}
+    >
+      <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
         <Link href="/">
-          <div>
-            <div>
+          <div className="flex flex-row items-center justify-center space-x-2">
+            <div className="ml-2 rounded-lg bg-[#3c4fe0] p-1">
               <Image
                 width={32}
                 height={32}
@@ -80,6 +84,14 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
             <p className="text-xl font-semibold text-white">ProteinBind</p>
           </div>
         </Link>
+
+        <button
+          onClick={() => setSidebarOpen(!sidebarOpen)}
+          aria-controls="sidebar"
+          className="block lg:hidden"
+        >
+          <ChevronLeft />
+        </button>
       </div>
     </aside>
   );
