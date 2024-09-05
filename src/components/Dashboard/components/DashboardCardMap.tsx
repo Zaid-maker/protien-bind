86026342b6361559330fa79930d26e388616c9d4
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-09-05 17:33:08 +0000

    FIx maps

--- src/components/Dashboard/components/DashboardCardMap.tsx
+++ src/components/Dashboard/components/DashboardCardMap.tsx
@@ -3,53 +3,53 @@
 import jsVectorMap from "jsvectormap";
 import "jsvectormap/dist/jsvectormap.css";
 import React, { useEffect } from "react";
-// import "../../../js/us-aea-en";
-// import "../../../js/world";
+import "../../../js/us-aea-en";
+import "../../../js/world";
 
 const MapOne: React.FC = () => {
-  // useEffect(() => {
-  //   const mapOne = new jsVectorMap({
-  //     selector: "#mapOne",
-  //     map: "world",
-  //     zoomButtons: true,
+  useEffect(() => {
+    const mapOne = new jsVectorMap({
+      selector: "#mapOne",
+      map: "world",
+      zoomButtons: true,
 
-  //     regionStyle: {
-  //       initial: {
-  //         fill: "#C8D0D8",
-  //       },
-  //       hover: {
-  //         fillOpacity: 1,
-  //         fill: "#3056D3",
-  //       },
-  //     },
-  //     regionLabelStyle: {
-  //       initial: {
-  //         fontFamily: "Satoshi",
-  //         fontWeight: "semibold",
-  //         fill: "#fff",
-  //       },
-  //       hover: {
-  //         cursor: "pointer",
-  //       },
-  //     },
+      regionStyle: {
+        initial: {
+          fill: "#C8D0D8",
+        },
+        hover: {
+          fillOpacity: 1,
+          fill: "#3056D3",
+        },
+      },
+      regionLabelStyle: {
+        initial: {
+          fontFamily: "Satoshi",
+          fontWeight: "semibold",
+          fill: "#fff",
+        },
+        hover: {
+          cursor: "pointer",
+        },
+      },
 
-  //     labels: {
-  //       regions: {
-  //         render(code: string) {
-  //           return code.split("-")[1];
-  //         },
-  //       },
-  //     },
-  //   });
+      labels: {
+        regions: {
+          render(code: string) {
+            return code.split("-")[1];
+          },
+        },
+      },
+    });
 
-  //   return () => {
-  //     const map = document.getElementById("mapOne");
-  //     if (map) {
-  //       map.innerHTML = "";
-  //     }
-  //     // mapOne.destroy();
-  //   };
-  // }, []);
+    return () => {
+      const map = document.getElementById("mapOne");
+      if (map) {
+        map.innerHTML = "";
+      }
+      // mapOne.destroy();
+    };
+  }, []);
 
   return (
     <div className="col-span-12 rounded-lg border border-stroke bg-white px-7.5 py-6 shadow-default dark:border-[#181818] dark:bg-[#181818] xl:col-span-7">
