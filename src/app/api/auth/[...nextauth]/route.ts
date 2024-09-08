f149392a89d0a94b9b3ab987cdf5260d58987296
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-09-08 16:30:56 +0000

    add callbacks and secret

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,6 +6,22 @@ export const handler = nextAuth({
     strategy: "jwt",
     maxAge: 24 * 60 * 60,
   },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+
+      return token;
+    },
+    async session({ session, token }: any) {
+      if (token) {
+        session.id = token.id;
+      }
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 });
 
 export { handler as GET, handler as POST };
